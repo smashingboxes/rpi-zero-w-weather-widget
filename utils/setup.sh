9d4a263cb9f2bed5f776a845cb80687db147fb83
Keegan Brown <brown.keegan@gmail.com>
2017-05-05 14:15:52 -0500

    Fix setup script

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,9 +1,8 @@
 echo "setup! cloning down and building wiringPi lib now."
-cd
+cd ~/
 git clone git://git.drogon.net/wiringPi
 cd ~/wiringPi
 git pull origin
-cd ~/wiringPi
 ./build
 gpio -v
 gpio readall
