0e5ce9aadcfbbafdd76ca985b703dcd9db772e0e
Keegan Brown <brown.keegan@gmail.com>
2017-05-05 14:14:13 -0500

    Adds wiring pi lib setup script

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,2 +1,9 @@
-echo "setup!"
-
+echo "setup! cloning down and building wiringPi lib now."
+cd
+git clone git://git.drogon.net/wiringPi
+cd ~/wiringPi
+git pull origin
+cd ~/wiringPi
+./build
+gpio -v
+gpio readall
